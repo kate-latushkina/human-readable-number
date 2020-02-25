22b16540b8f873d53eb49edd872828bfcb652502
kate-latushkina <59828320+kate-latushkina@users.noreply.github.com>
2020-02-25 14:27:46 +0200

    feat: add correct solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function toReadable(number) {
     const num = {
-        0: 'zero',
         1: 'one',
         2: 'two',
         3: 'three',
@@ -31,12 +30,35 @@ module.exports = function toReadable(number) {
         100: 'hundred',
     }
     for (let key in num) {
-        if (num[key] == number) {
-            console.log(num[key]);
+        if (key == number && key != 100) {
+            return num[key];
+        }
+        else {
+            if (number == 0) {
+                return 'zero';
+            }
+            else if (number > 20 && number < 100 && number !== 20 && number !== 30 && number !== 40 && number !== 50 && number !== 60 && number !== 70 && number !== 80 && number !== 90) {
+                let arrNum = number.toString().split('');
+                return `${num[arrNum[0] * 10]} ${num[arrNum[1]]}`;
+            }
+            else if (number == 100 || number == 200 || number == 300 || number == 400 || number == 500 || number == 600 || number == 700 || number == 800 || number == 900) {
+                return `${num[number / 100]} ${num[100]}`;
+            }
+            else if (number > 100 && number !== 200 && number !== 300 && number !== 400 && number !== 500 && number !== 600 && number !== 700 && number !== 800 && number !== 900) {
+                let arrNum = number.toString().split('');
+                if (arrNum[1] == 1) {
+                    return `${num[arrNum[0]]} ${num[100]} ${num[+arrNum[2] + 10]}`;
+                }
+                else if (arrNum[2] == 0) {
+                    return `${num[arrNum[0]]} ${num[100]} ${num[arrNum[1] * 10]}`;
+                }
+                else if (arrNum[1] == 0 && arrNum.length >2) {
+                    return `${num[arrNum[0]]} ${num[100]} ${num[arrNum[2]]}`;
+                }
+                else {
+                    return `${num[arrNum[0]]} ${num[100]} ${num[arrNum[1] * 10]} ${num[arrNum[2]]}`;
+                }
+            }
         }
     }
 }
-
-toReadable(5)
-toReadable(100)
-toReadable(17)
